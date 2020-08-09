79917790f37f49d683e4ffbbb691f8161c10456e
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-08-08 21:28:42 -0700

    added build file

---
+++ mywebsite/build/precache-manifest.f4e2b4dc60da29f94c9661e39ade3df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b565a7b1c8d4ed8b3257fe9a1e6b29c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "818570a9a95c5c58abb4",
+    "url": "/static/css/main.d8da7e81.chunk.css"
+  },
+  {
+    "revision": "210b445874a5d60f0597",
+    "url": "/static/js/2.6d255b2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d255b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818570a9a95c5c58abb4",
+    "url": "/static/js/main.0976bee0.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  }
+]);
