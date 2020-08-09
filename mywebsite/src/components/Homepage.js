8ee8c574f84b2aa769d6852748d4141673dfd012
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-08-08 20:49:39 -0700

    minor changes

--- mywebsite/src/components/Homepage.js
+++ mywebsite/src/components/Homepage.js
@@ -4,7 +4,9 @@ const Homepage = () => {
 
     return (
         <div>
-            <h1>Homepage</h1>
+            <h1>Welcome to my nook</h1>
+            <p>My name is Alex and I am an undergraduate computer science student.</p>
+            <p>I have a passion for coding and I love spending my free time developing beautiful React apps.</p> 
         </div>
     )
 }
